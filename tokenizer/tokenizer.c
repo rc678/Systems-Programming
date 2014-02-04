e28889db65fca0bfba60dfad545158ad17f5b3dd
rc678 <rc678@eden.rutgers.edu>
2014-02-03 23:18:36 -0500

    Update tokenizer.c

--- tokenizer/tokenizer.c
+++ tokenizer/tokenizer.c
@@ -1,14 +1,14 @@
 /*
- * tokenizer.c
- */
+ *  * tokenizer.c
+ *   */
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 
 
 /*
- * Tokenizer type.  You need to fill in the type as part of your implementation.
- */
+ *  * Tokenizer type.  You need to fill in the type as part of your implementation.
+ *   */
 struct TokenizerT_ {/*implemented as a Linked List*/
 	char* token;
 	struct TokenizerT_* nextToken;
@@ -22,15 +22,15 @@ char* substring(char*, int, int);
 char* checkEscape(char*, int, int, char*, int);
 int checkSep(char*, int); 
 /*
- * TKCreate creates a new TokenizerT object for a given set of separator
- * characters (given as a string) and a token stream (given as a string).
- * TKCreate should copy the two arguments so that it is not dependent on
- * them staying immutable after returning.  (In the future, this may change
- * to increase efficiency.)
- * If the function succeeds, it returns a non-NULL TokenizerT.
- * Else it returns NULL.
- * You need to fill in this function as part of your implementation.
- */
+ *  * TKCreate creates a new TokenizerT object for a given set of separator
+ *   * characters (given as a string) and a token stream (given as a string).
+ *    * TKCreate should copy the two arguments so that it is not dependent on
+ *     * them staying immutable after returning.  (In the future, this may change
+ *      * to increase efficiency.)
+ *       * If the function succeeds, it returns a non-NULL TokenizerT.
+ *        * Else it returns NULL.
+ *         * You need to fill in this function as part of your implementation.
+ *          */
 
 TokenizerT *TKCreate(char *separators, char *ts) {
 	TokenizerT* head;
@@ -56,8 +56,8 @@ TokenizerT *TKCreate(char *separators, char *ts) {
 	strcpy(sep, separators);
 
 	/*1st for loop traverses through the given string character by character. 
-	 * 2nd for loop checks to see if the character is a separator character. 
-	 */
+	 	 * 2nd for loop checks to see if the character is a separator character. 
+	  	 	 */
 	for(currIndex = 0; currIndex < strLen; currIndex++)
 	{
 		for(j = 0; j < sepLen; j++)
@@ -131,12 +131,12 @@ TokenizerT *TKCreate(char *separators, char *ts) {
 }
 
 /*CheckEscape accepts a character string, the index at which a backslash occurs, a string of separator
- * characters, and the lengths of the two strings. It then checks if the backlash is part of an escape character
- *  or if it is simply a single backslash.
- * In the first case, it returns a string of proper length that has the hex form of the escape character in
- * question instead of the escape character itself. In the latter case, it returns a string of proper length
- * in which the backslash is removed.
- */
+ *  * characters, and the lengths of the two strings. It then checks if the backlash is part of an escape character
+ *   *  or if it is simply a single backslash.
+ *    * In the first case, it returns a string of proper length that has the hex form of the escape character in
+ *     * question instead of the escape character itself. In the latter case, it returns a string of proper length
+ *      * in which the backslash is removed.
+ *       */
 char* checkEscape(char* string, int currIndex, int strLen, char* sep, int sepLen)
 {
 	char str[strLen];
@@ -262,20 +262,15 @@ char* checkEscape(char* string, int currIndex, int strLen, char* sep, int sepLen
 		resultIndex++; 
 	}
 	result[strLen+4] = '\0'; 	
-	for(i = 0; i < strlen(result); i++)
-	{
-		printf("char at %d is %c in checkSpace\n", i, result[i]);	
-	}
 	size = strLen + 5;
 	res = malloc(size);
 	strcpy(res, result);
-	/*	printf("res is %s\n", res);*/
 	return res; 
 
 }/*end of checkEscape method*/
 
 /*If the argument contains a '\' character, checkSep checks if this is a backslash character or
- *  * part of an escape character*/
+ * part of an escape character*/
 int checkSep(char* sep, int sepLen)
 {
 	int i;
@@ -313,11 +308,10 @@ int checkSep(char* sep, int sepLen)
 }
 
 /*
- * 
- * Takes in the input string, index of the 1st character of the token in the input string, and 
- * index of the last character of the token in the input string. Uses these variables to dynamically allocate 
- * the token string. The function returns a pointer to the dynamically allocated string. 
- */
+    * Takes in the input string, index of the 1st character of the token in the input string, and 
+     * index of the last character of the token in the input string. Uses these variables to dynamically allocate 
+      * the token string. The function returns a pointer to the dynamically allocated string. 
+       */
 char* substring(char* string, int start, int end)
 {
 	int strLen = strlen(string);
@@ -331,8 +325,8 @@ char* substring(char* string, int start, int end)
 
 	curr = 0;
 	/*for loop goes to the start index of the new token in the given string and copies the characters
-	 * into a new array until the end index in the input string. These characters are a token.  
-	 */
+	  	 * into a new array until the end index in the input string. These characters are a token.  
+	  	 	 */
 	for(i = start; i <= end+1; i++)
 	{
 		result[curr] = str[i];
@@ -347,10 +341,10 @@ char* substring(char* string, int start, int end)
 
 
 /*
- * TKDestroy destroys a TokenizerT object.  It should free all dynamically
- * allocated memory that is part of the object being destroyed.
- * You need to fill in this function as part of your implementation.
- */
+   * TKDestroy destroys a TokenizerT object.  It should free all dynamically
+    * allocated memory that is part of the object being destroyed.
+     * You need to fill in this function as part of your implementation.
+      */
 
 void TKDestroy(TokenizerT *tk) {
 
@@ -365,14 +359,14 @@ void TKDestroy(TokenizerT *tk) {
 }/*end of TKDestory method*/
 
 /*
- * TKGetNextToken returns the next token from the token stream as a
- * character string.  Space for the returned token should be dynamically
- * allocated.  The caller is responsible for freeing the space once it is
- * no longer needed.
- * If the function succeeds, it returns a C string (delimited by '\0')
- * containing the token.  Else it returns 0.
- * You need to fill in this function as part of your implementation.
- */
+   * TKGetNextToken returns the next token from the token stream as a
+    * character string.  Space for the returned token should be dynamically
+     * allocated.  The caller is responsible for freeing the space once it is
+      * no longer needed.
+       * If the function succeeds, it returns a C string (delimited by '\0')
+        * containing the token.  Else it returns 0.
+         * You need to fill in this function as part of your implementation.
+          */
 
 char *TKGetNextToken(TokenizerT *tk) {
 
@@ -394,12 +388,12 @@ char *TKGetNextToken(TokenizerT *tk) {
 
 
 /*
- * main will have two string arguments (in argv[1] and argv[2]).
- * The first string contains the separator characters.
- * The second string contains the tokens.
- * Print out the tokens in the second string in left-to-right order.
- * Each token should be printed on a separate line.
- */
+   * main will have two string arguments (in argv[1] and argv[2]).
+    * The first string contains the separator characters.
+     * The second string contains the tokens.
+      * Print out the tokens in the second string in left-to-right order.
+       * Each token should be printed on a separate line.
+        */
 
 int main(int argc, char **argv) {
 
