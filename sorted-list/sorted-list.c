b60c1eef3e9c564b9c0157143c3e4d05aab00c32
rc678 <rc678@eden.rutgers.edu>
2014-02-09 23:55:35 -0500

    Original sorted-list.c file

---
+++ sorted-list/sorted-list.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sorted-list.h"
+#include "main.c"
+
+SortedListPtr SLCreate(CompareFuncT cf)
+{
+
+	return NULL;
+}
+
+void SLDestroy(SortedListPtr list)
+{
+	
+}
+
+int SLInsert(SortedListPtr list, void* newObj)
+{
+
+	return 0;
+}
+
+int SLRemove(SortedListPtr list, void* newObj)
+{
+	return 0;
+}
+
+SortedListIteratorPtr SLCreateIterator(SortedListPtr list)
+{
+	return NULL;
+}
+
+void SLDestoryIterator(SortedListIteratorPtr iter)
+{
+
+}
+
+void* SLNextItem(SortedListIteratorPtr iter)
+{
+	return NULL;
+}
