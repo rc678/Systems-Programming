9525afd8faf3a22a8e4e0b2873d570e22a74a98e
rc678 <rc678@eden.rutgers.edu>
2014-02-13 15:24:21 -0500

    finished SLcreate and SLcreateIterator

--- sorted-list/sorted-list.c
+++ sorted-list/sorted-list.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "sorted-list.h"
-#include "main.c"
+
 
 SortedListPtr SLCreate(CompareFuncT cf)
 {
 
+	SortedListPtr list; 
+	if(cf != NULL)
+	{
+		list = (SortedListPtr)malloc(sizeof(struct SortedList));
+		list->head = NULL;
+		list->cf = cf;
+	}
 	return NULL;
 }
 
@@ -15,8 +22,17 @@ void SLDestroy(SortedListPtr list)
 }
 
 int SLInsert(SortedListPtr list, void* newObj)
-{
+{	
 
+	if(list->head == NULL)/*if the Linked List is empty. Creates a node and sets it as the head*/
+	{
+		nodePtr temp = malloc(sizeof(node));
+		temp->data = newObj;
+		temp->counter++;
+		temp->next = NULL;
+		list->head = temp;
+		return 1;
+	}
 	return 0;
 }
 
@@ -27,6 +43,15 @@ int SLRemove(SortedListPtr list, void* newObj)
 
 SortedListIteratorPtr SLCreateIterator(SortedListPtr list)
 {
+	SortedListIteratorPtr itPtr = (SortedListIteratorPtr)malloc(sizeof(struct SortedListIterator));
+
+	if(list->head != NULL)
+	{
+		itPtr->curr = list->head;
+		list->head->counter++;
+		return itPtr; 
+			
+	}		
 	return NULL;
 }
 
