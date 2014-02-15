b25a7bee65b09617825bb4b270df6f09ef8231b5
davidrubin9 <djr190@eden.rutgers.edu>
2014-02-15 16:21:24 -0500

    Temp update in main
    added a bunch of numbers to test insert and make sure it adds them in the right order. Seems to be working. We should make it so we can just scan input instead of having to compile the program every time we test it with different numbers.

--- sorted-list/main.c
+++ sorted-list/main.c
@@ -33,13 +33,49 @@ int compareStrings(void *p1, void *p2)
 int main()
 {
 	printf("in main\n");
-	SortedListPtr list = SLCreate(compareInts);	
-	printf("after list\n");
+        SortedListPtr list = SLCreate(compareInts);    
+        printf("after list\n");
+        int z;  
 
-	int* a = (int*) malloc(sizeof(double));
-	*a = 12;
-	SLInsert(list, (void*)a);
+        int* a = (int*) malloc(sizeof(double));
+        int* b = (int*) malloc(sizeof(double));
+        int* c = (int*) malloc(sizeof(double));
+        int* d = (int*) malloc(sizeof(double));
+        int* e = (int*) malloc(sizeof(double));
+        int* f = (int*) malloc(sizeof(double));
+        int* g = (int*) malloc(sizeof(double));
 
-	printf("after insert\n");
-	return 0;
+        *a = 11; 
+        z = SLInsert(list, (void*)a);
+        printf("%d\n", z); 
+        *b = 20;
+        z = SLInsert(list, (void*)b);
+        printf("%d\n", z); 
+        *c = 5;
+        z = SLInsert(list, (void*)c);
+        printf("%d\n", z); 
+        *d = -8;
+        z = SLInsert(list, (void*)d);
+        printf("%d\n", z);
+        *e = 9;
+        z = SLInsert(list, (void*)e);
+        printf("%d\n", z);
+        *f = 11;
+        z = SLInsert(list, (void*)f);
+        printf("%d\n", z);
+        *g = 52;
+        z = SLInsert(list, (void*)g);
+        printf("%d\n", z);
+
+
+        printf("after insert\n");
+
+        nodePtr ptr = list->head;
+
+        while (ptr != NULL){
+                printf("%d\n", *(int*) ptr->data);
+                ptr = ptr->next;
+        }
+
+        return 0;
 }
