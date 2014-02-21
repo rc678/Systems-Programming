cab27aa7664116d7e3965d99c26623e15d4c7c2a
davidrubin9 <djr190@eden.rutgers.edu>
2014-02-20 23:44:12 -0500

    New Update
    most functions are now well-commented. A few minor fixes i didn't think of before. Added destruct function initialization to SLCreate. The name of SLDestroyIterator was misspelled before too

--- sorted-list/sorted-list.c
+++ sorted-list/sorted-list.c
@@ -3,81 +3,119 @@
 #include "sorted-list.h"
 
 
-SortedListPtr SLCreate(CompareFuncT cf)
+SortedListPtr SLCreate(CompareFuncT cf, DestructFuncT df)
 {
 
 	printf("in create\n");
 	SortedListPtr list; 
-	if(cf != NULL)
+	
+	/*Malloc space for a SortedListPtr, initialize it, and return it as long as cf and df are non-NULL*/
+	if(cf != NULL && df != NULL)
 	{
 		list = (SortedListPtr)malloc(sizeof(struct SortedList));
 		list->head = NULL;
 		list->cf = cf;
+		list->df = df;
 		return list;
 	}
-	return NULL;
+	return NULL; /*return NULL otherwise*/
 }
 
 void SLDestroy(SortedListPtr list)
 {
-	nodePtr curr = list->head;
-        nodePtr temp = curr->next;
+	/*Simply return if the list is a NULL pointer*/
+	if (list == NULL){
+		return;
+	}
+	
+	nodePtr curr = list->head; /*node pointer for node getting freed*/
+        nodePtr temp; /*temp node pointer for next node so the previous node can be freed*/
+	
+	/*If the list is empty, free the list and return*/
+	if (curr == NULL){
+		free(list)
+		return;
+	}
+
+	temp = curr->next;
+
+	/*Free all nodes until there is only one non-NULL node left pointed to by curr*/
         while (temp != NULL){
                 free(curr);
                 curr = temp;
                 temp = curr->next;
         }   
-
-        free(curr);
-        free(list);	
+        
+        free(curr); /*Free last node*/
+        free(list); /*Free the sorted list object created by SLCreate*/
 }
 
 int SLInsert(SortedListPtr list, void* newObj)
 {	
 	printf("in sorted list and new object is %d\n", *(int*) newObj);
+        
+        /*return 0 if a NULL object is being inserted*/
+        if (newObj == NULL){
+        	return 0;
+        }
+
         nodePtr newNode = (nodePtr)malloc(sizeof(node));
         newNode->data = newObj;
         nodePtr ptr = list->head;
         int compareValue;
 
-        if (ptr == NULL){
+	/*Add the new object and return 1 automatically if the sorted list is empty*/ 
+	if (ptr == NULL){
                 list->head = newNode;
-                printf("%d\n", newNode->numPtrs);
-                newNode->numPtrs++;
+                newNode->numPtrs++; 
+                newNode->isInList = 1; 
                 return 1;
         }   
 
-        compareValue = (*list->cf)(newObj, ptr->data);
+        compareValue = (*list->cf)(newObj, ptr->data); /*compare the new object with the data in head*/
 
+	/*If the new data is the same as the data in list->head, free the newly malloced object
+	and return 1 since this is a duplicate*/
         if (compareValue == 0){ 
                 free(newNode);
                 return 0;
         }    
 
+	/*IF the new data is greater than the data in list->head, set the new node as the new head
+	of the list and return 1. No further comparisons are required*/
         if (compareValue > 0){ 
                 newNode->next = ptr;
                 list->head = newNode;
                 newNode->numPtrs++;
+                newNode->isInList = 1;
                 return 1;
         }    
 
+	/*If there is only one object in the list and it is greater than the new data, add the new
+	node to the end of the list and return 1*/
         if (ptr->next == NULL){
                 ptr->next = newNode;
                 newNode->numPtrs++;
+                newNode->isInList = 1;
                 return 1;
         }   
 
-        compareValue = list->cf(newObj, ptr->next->data);
+        compareValue = list->cf(newObj, ptr->next->data); /*now compare the data with the second node*/
 
+	/*Move down the list until the new data is greater than the data in ptr->next node. Then
+	add the new node to the list between ptr and ptr->next*/
         while (compareValue <= 0){ 
 
                 ptr = ptr->next;
+                
+                /*Return 0 and free the newNode if a duplicate is found*/
                 if (compareValue == 0){ 
                         free(newNode);
-                        printf("duplicate value\n");
                         return 0;
                 }   
 
+		/*If the end of the list is reached, break the loop and and the newNode
+		to the end automatically to avoid a segmentation fault*/
                 if (ptr->next == NULL){
                         break;
                 }   
@@ -88,6 +126,7 @@ int SLInsert(SortedListPtr list, void* newObj)
         newNode->next = ptr->next;
         ptr->next = newNode;
         newNode->numPtrs++;
+        newNode->isInList = 1;
         return 1;
 }
 
@@ -205,11 +244,17 @@ SortedListIteratorPtr SLCreateIterator(SortedListPtr list)
 	return NULL;
 }
 
-void SLDestoryIterator(SortedListIteratorPtr iter)
+void SLDestroyIterator(SortedListIteratorPtr iter)
 {
-	nodePtr temp1 = iter->curr;
-        nodePtr temp2;
+	/*Simply return if iter is a NULL pointer*/
+	if (iter == NULL){
+		return;
+	}
+	
+	nodePtr temp1 = iter->curr; /*node pointer to node being freed*/
+        nodePtr temp2; /*temp node pointer for next node so the previous node can be freed*/
 
+	/*If the first node pointed to by iterator is NULL, free the iterator struct and return*/
         if (temp1 == NULL){
                 free(iter);
                 return;
@@ -217,11 +262,16 @@ void SLDestoryIterator(SortedListIteratorPtr iter)
 
         temp2 = temp1->next;
 
-        if (temp2 == NULL && temp1,->numPtrs > 1){ 
+	/*If the iterator points to a single node list but the node has other pointers pointing to it,
+	simply free the iterator and return*/
+        if (temp2 == NULL && temp1->numPtrs > 1){ 
                 free(iter);
+                temp1->numPtrs--;
                 return;
         }    
 
+ 	/*While iter->curr points to a non-NULL node, move the iterator down the list, freeing each node
+ 	along the way, until a node is found to be part of another iterator or the sorted list*/
         while (temp2 != NULL)
         {   
                 temp1->numPtrs--;
@@ -234,9 +284,14 @@ void SLDestoryIterator(SortedListIteratorPtr iter)
                 temp1 = temp2;
                 temp2 = temp1->next;
         }   
-            
-        free(temp1);
-        free(iter);
+        
+        temp1->numPtrs--;    
+        
+        /*free last node only if nothing else points to it*/
+	if (temp1->numPtrs == 0){
+        	free(temp1);
+	}
+        free(iter); /*free iterator*/
 }
 
 void* SLNextItem(SortedListIteratorPtr iter)
@@ -249,7 +304,9 @@ void* SLNextItem(SortedListIteratorPtr iter)
 		printf("list is empty\n");
 		return NULL;
 	}
+	
 	iter->curr->numPtrs--;
+	
 	if(iter->curr->numPtrs == 0)
 	{
 		free(iter->curr);
