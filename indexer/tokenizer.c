938247e93e1472b5cfa72f958ccb02690376e10b
davidrubin9 <djr190@eden.rutgers.edu>
2014-03-04 02:52:03 -0500

    Updated Tokenizer to sort list of files
    Changed "node" struct to "record" struct. Also, two functions were added. updateRecordList() looks to see if the current file is already in the list for this token. If not, it adds it to the end. If so, it increments the frequency and calls resortRecordList(). This function re-sorts the record list as necessary. I haven't tested it yet bc i haven't saved the test case files/directories to test with. But it does compile and I did a thorough test on paper with drawings for many different cases and I think it should work.

--- indexer/tokenizer.c
+++ indexer/tokenizer.c
@@ -1,6 +1,6 @@
 /*
- * * Tokenizer.c
- * */
+ * Tokenizer.c
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -11,21 +11,26 @@
 #define MAX_OCT_CHARS 3
 
 
-typedef struct node{
+typedef struct record{
 	char* fileName;
 	int frequency;
-	struct node* next;
+	struct record *next;
+	struct record *prev;
 
-}node, *nodePtr;
+}record, *recordPtr;
 
 struct my_struct{
 	const char* word; 
-	nodePtr list;
+	recordPtr list;
 	UT_hash_handle hh;
 };
+
+recordPtr updateRecordList(recordPtr, char*);
+recordPtr resortRecordList(recordPtr, recordPtr);
+
 /*
- *  *  * Tokenizer type.  You need to fill in the type as part of your implementation.
- *   *   */
+ *  *  *  * Tokenizer type.  You need to fill in the type as part of your implementation.
+ *   *   *   */
 
 struct TokenizerT_ {
 	char* copied_string;		
@@ -37,12 +42,12 @@ typedef struct TokenizerT_ TokenizerT;
 char is_escape_character(char character) {
 
 	/*
-	 *  * 	 * Description: determines if a character is a member of the set of escaped characters
-	 *   * 	 	 * Parameters: character to be evaluated
-	 *    * 	 	 	 * Modifies: nothing
-	 *     * 	 	 	 	 * Returns: appropriate escape character sequence if it is a member, 0 if it is not
-	 *      * 	 	 	 	 	 * 
-	 *       * 	 	 	 	 	 	 */
+	 * 	 *  * 	 * Description: determines if a character is a member of the set of escaped characters
+	 * 	 	 *   * 	 	 * Parameters: character to be evaluated
+	 * 	 	 	 *    * 	 	 	 * Modifies: nothing
+	 * 	 	 	 	 *     * 	 	 	 	 * Returns: appropriate escape character sequence if it is a member, 0 if it is not
+	 * 	 	 	 	 	 *      * 	 	 	 	 	 * 
+	 * 	 	 	 	 	 	 *       * 	 	 	 	 	 	 */
 
 	char* escape_sequence = "ntvbrfa\\?'\"";
 	char* escape_characters = "\n\t\v\b\r\f\a\\\?\'\"";
@@ -60,12 +65,12 @@ char is_escape_character(char character) {
 int char_to_hex(char character) {
 
 	/*
-	 *  * 	 * Description: converts a hex digit represented as a character into its actual integer value
-	 *   * 	 	 * Parameters: character to be converted
-	 *    * 	 	 	 * Modifies: nothing
-	 *     * 	 	 	 	 * Returns: value of the hex digit character as an integer
-	 *      * 	 	 	 	 	 * 
-	 *       * 	 	 	 	 	 	 */
+	 * 	 *  * 	 * Description: converts a hex digit represented as a character into its actual integer value
+	 * 	 	 *   * 	 	 * Parameters: character to be converted
+	 * 	 	 	 *    * 	 	 	 * Modifies: nothing
+	 * 	 	 	 	 *     * 	 	 	 	 * Returns: value of the hex digit character as an integer
+	 * 	 	 	 	 	 *      * 	 	 	 	 	 * 
+	 * 	 	 	 	 	 	 *       * 	 	 	 	 	 	 */
 
 	if(isdigit(character)){
 		return character - '0';
@@ -79,12 +84,12 @@ int char_to_hex(char character) {
 int char_to_oct(char character) {
 
 	/*
-	 *  * 	 * Description: converts a octal digit represented as a character into its actual integer value
-	 *   * 	 	 * Parameters: character to be converted
-	 *    * 	 	 	 * Modifies: nothing
-	 *     * 	 	 	 	 * Returns: value of the octal digit character as an integer
-	 *      * 	 	 	 	 	 * 
-	 *       * 	 	 	 	 	 	 */
+	 * 	 *  * 	 * Description: converts a octal digit represented as a character into its actual integer value
+	 * 	 	 *   * 	 	 * Parameters: character to be converted
+	 * 	 	 	 *    * 	 	 	 * Modifies: nothing
+	 * 	 	 	 	 *     * 	 	 	 	 * Returns: value of the octal digit character as an integer
+	 * 	 	 	 	 	 *      * 	 	 	 	 	 * 
+	 * 	 	 	 	 	 	 *       * 	 	 	 	 	 	 */
 
 	return character - '0';
 } 
@@ -92,12 +97,12 @@ int char_to_oct(char character) {
 int is_oct_digit(char oct_digit) {
 
 	/*
-	 *  * 	 * Description: determines if a character represents a octal digit
-	 *   * 	 	 * Parameters: character to be evaluated
-	 *    * 	 	 	 * Modifies: nothing
-	 *     * 	 	 	 	 * Returns: 1 if it is an octal character, 0 if it is not
-	 *      * 	 	 	 	 	 * 
-	 *       * 	 	 	 	 	 	 */
+	 * 	 *  * 	 * Description: determines if a character represents a octal digit
+	 * 	 	 *   * 	 	 * Parameters: character to be evaluated
+	 * 	 	 	 *    * 	 	 	 * Modifies: nothing
+	 * 	 	 	 	 *     * 	 	 	 	 * Returns: 1 if it is an octal character, 0 if it is not
+	 * 	 	 	 	 	 *      * 	 	 	 	 	 * 
+	 * 	 	 	 	 	 	 *       * 	 	 	 	 	 	 */
 
 	if(oct_digit >= '0' && oct_digit <= '7') {
 		return 1;
@@ -110,12 +115,12 @@ int is_oct_digit(char oct_digit) {
 char* unescape_string(char* string) {
 
 	/*
-	 *  * 	 * Description: given an input stream converts escaped characters into their appropriate char representation and returns it as a new string
-	 *   * 	 	 * Parameters: string to be converted
-	 *    * 	 	 	 * Modifies: nothing
-	 *     * 	 	 	 	 * Returns: resulting string with all the escaped characters with their appropriate values
-	 *      * 	 	 	 	 	 * 
-	 *       * 	 	 	 	 	 	 */
+	 * 	 *  * 	 * Description: given an input stream converts escaped characters into their appropriate char representation and returns it as a new string
+	 * 	 	 *   * 	 	 * Parameters: string to be converted
+	 * 	 	 	 *    * 	 	 	 * Modifies: nothing
+	 * 	 	 	 	 *     * 	 	 	 	 * Returns: resulting string with all the escaped characters with their appropriate values
+	 * 	 	 	 	 	 *      * 	 	 	 	 	 * 
+	 * 	 	 	 	 	 	 *       * 	 	 	 	 	 	 */
 
 	char* unescaped_string = (char*)malloc(strlen(string) * sizeof(char) + 1);
 	int current_position = 0;
@@ -168,28 +173,28 @@ char* unescape_string(char* string) {
 }
 
 /*
- *  *  * TKCreate creates a new TokenizerT object for a given set of separator
- *   *   * characters (given as a string) and a token stream (given as a string).
- *    *    * 
- *     *     * TKCreate should copy the two arguments so that it is not dependent on
- *      *      * them staying immutable after returning.  (In the future, this may change
- *       *       * to increase efficiency.)
- *        *        *
- *         *         * If the function succeeds, it returns a non-NULL TokenizerT.
- *          *          * Else it returns NULL.
- *           *           *
- *            *            * You need to fill in this function as part of your implementation.
- *             *             */
+ *  *  *  * TKCreate creates a new TokenizerT object for a given set of separator
+ *   *   *   * characters (given as a string) and a token stream (given as a string).
+ *    *    *    * 
+ *     *     *     * TKCreate should copy the two arguments so that it is not dependent on
+ *      *      *      * them staying immutable after returning.  (In the future, this may change
+ *       *       *       * to increase efficiency.)
+ *        *        *        *
+ *         *         *         * If the function succeeds, it returns a non-NULL TokenizerT.
+ *          *          *          * Else it returns NULL.
+ *           *           *           *
+ *            *            *            * You need to fill in this function as part of your implementation.
+ *             *             *             */
 
 TokenizerT *TKCreate(char *ts) {
 
 	/*
-	 * 	 * 	 * Description: creates a new tokenizer struct from the token stream and delimiters
-	 * 	 	 * 	 	 * Parameters: set of delimiters, token stream
-	 * 	 	 	 * 	 	 	 * Modifies: nothing
-	 * 	 	 	 	 * 	 	 	 	 * Returns: a pointer to a tokenizer struct on success, a null pointer on failure
-	 * 	 	 	 	 	 * 	 	 	 	 	 * 
-	 * 	 	 	 	 	 	 * 	 	 	 	 	 	 */
+	 * 	 * 	 * 	 * Description: creates a new tokenizer struct from the token stream and delimiters
+	 * 	 	 * 	 	 * 	 	 * Parameters: set of delimiters, token stream
+	 * 	 	 	 * 	 	 	 * 	 	 	 * Modifies: nothing
+	 * 	 	 	 	 * 	 	 	 	 * 	 	 	 	 * Returns: a pointer to a tokenizer struct on success, a null pointer on failure
+	 * 	 	 	 	 	 * 	 	 	 	 	 * 	 	 	 	 	 * 
+	 * 	 	 	 	 	 	 * 	 	 	 	 	 	 * 	 	 	 	 	 	 */
 
 	if(ts == NULL){
 		return NULL;
@@ -208,20 +213,20 @@ TokenizerT *TKCreate(char *ts) {
 }
 
 /*
- *  *  * TKDestroy destroys a TokenizerT object.  It should free all dynamically
- *   *   * allocated memory that is part of the object being destroyed.
- *    *    *
- *     *     * You need to fill in this function as part of your implementation.
- *      *      */
+ *  *  *  * TKDestroy destroys a TokenizerT object.  It should free all dynamically
+ *   *   *   * allocated memory that is part of the object being destroyed.
+ *    *    *    *
+ *     *     *     * You need to fill in this function as part of your implementation.
+ *      *      *      */
 
 void TKDestroy(TokenizerT *tk) {	
 
 	/*
-	 * 	 * 	 * Description: destroys tokenizer struct and deallocates all memory
-	 * 	 	 * 	 	 * Parameters: tokenizer to be destroyed
-	 * 	 	 	 * 	 	 	 * Modifies: tokenizer struct: deallocates memory
-	 * 	 	 	 	 * 	 	 	 	 * Returns: nothing 
-	 * 	 	 	 	 	 * 	 	 	 	 	 */
+	 * 	 * 	 * 	 * Description: destroys tokenizer struct and deallocates all memory
+	 * 	 	 * 	 	 * 	 	 * Parameters: tokenizer to be destroyed
+	 * 	 	 	 * 	 	 	 * 	 	 	 * Modifies: tokenizer struct: deallocates memory
+	 * 	 	 	 	 * 	 	 	 	 * 	 	 	 	 * Returns: nothing 
+	 * 	 	 	 	 	 * 	 	 	 	 	 * 	 	 	 	 	 */
 
 	free(tk->copied_string);
 	free(tk);
@@ -249,11 +254,11 @@ char* toLowerCase(char *token){
 char is_delimiter(char character) {
 
 	/*
-	 * 	 * 	 * Description: determines if a particular character is a member of the set of delimiters
-	 * 	 	 * 	 	 * Parameters: character to be compared, string of delimiters
-	 * 	 	 	 * 	 	 	 * Modifies: Nothing
-	 * 	 	 	 	 * 	 	 	 	 * Returns: 1 if character is a delimiter, 0 if it is not
-	 * 	 	 	 	 	 * 	 	 	 	 	 */
+	 * 	 * 	 * 	 * Description: determines if a particular character is a member of the set of delimiters
+	 * 	 	 * 	 	 * 	 	 * Parameters: character to be compared, string of delimiters
+	 * 	 	 	 * 	 	 	 * 	 	 	 * Modifies: Nothing
+	 * 	 	 	 	 * 	 	 	 	 * 	 	 	 	 * Returns: 1 if character is a delimiter, 0 if it is not
+	 * 	 	 	 	 	 * 	 	 	 	 	 * 	 	 	 	 	 */
 
 	int ch = character;
 
@@ -265,25 +270,25 @@ char is_delimiter(char character) {
 }
 
 /*
- *  *  * TKGetNextToken returns the next token from the token stream as a
- *   *   * character string.  Space for the returned token should be dynamically
- *    *    * allocated.  The caller is responsible for freeing the space once it is
- *     *     * no longer needed.
- *      *      *
- *       *       * If the function succeeds, it returns a C string (delimited by '\0')
- *        *        * containing the token.  Else it returns 0.
- *         *         *
- *          *          * You need to fill in this function as part of your implementation.
- *           *           */
+ *  *  *  * TKGetNextToken returns the next token from the token stream as a
+ *   *   *   * character string.  Space for the returned token should be dynamically
+ *    *    *    * allocated.  The caller is responsible for freeing the space once it is
+ *     *     *     * no longer needed.
+ *      *      *      *
+ *       *       *       * If the function succeeds, it returns a C string (delimited by '\0')
+ *        *        *        * containing the token.  Else it returns 0.
+ *         *         *         *
+ *          *          *          * You need to fill in this function as part of your implementation.
+ *           *           *           */
 
 char *TKGetNextToken(TokenizerT *tk) {
 
 	/*
-	 * 	 * 	 * Description: returns the next token from the token stream specified within the tokenizer
-	 * 	 	 * 	 	 * Parameters: tokenizer from which to extract token
-	 * 	 	 	 * 	 	 	 * Modifies: tokenizer->current_position: identifies starting point of next token; creates a new string with 
-	 * 	 	 	 	 * 	 	 	 	 * Returns: token extracted as a char* on success, null on failure/end of string;
-	 * 	 	 	 	 	 * 	 	 	 	 	 */
+	 * 	 * 	 * 	 * Description: returns the next token from the token stream specified within the tokenizer
+	 * 	 	 * 	 	 * 	 	 * Parameters: tokenizer from which to extract token
+	 * 	 	 	 * 	 	 	 * 	 	 	 * Modifies: tokenizer->current_position: identifies starting point of next token; creates a new string with 
+	 * 	 	 	 	 * 	 	 	 	 * 	 	 	 	 * Returns: token extracted as a char* on success, null on failure/end of string;
+	 * 	 	 	 	 	 * 	 	 	 	 	 * 	 	 	 	 	 */
 
 	char* token = NULL;
 	char* token_start = NULL;
@@ -318,8 +323,8 @@ char *TKGetNextToken(TokenizerT *tk) {
 
 
 /*This function will start the process of Tokenizing the string from the file
- *  * *It will return a 1 if successfully split and a 0 otherwise.
- *   * */ 
+ *  *  * *It will return a 1 if successfully split and a 0 otherwise.
+ *   *   * */ 
 int split(FILE* file, char* fileName) {
 
 	if(file == NULL)
@@ -329,7 +334,7 @@ int split(FILE* file, char* fileName) {
 	}
 
 	char* token;
-	nodePtr temp; 
+	recordPtr temp; 
 	struct my_struct *s, *words = NULL;
 
 	char str[1000000];
@@ -339,45 +344,33 @@ int split(FILE* file, char* fileName) {
 	TokenizerT* t = TKCreate(st);
 	token = TKGetNextToken(t);
 
-	HASH_FIND_STR(words, token, s);
-	if(!s)
-	{
-		s = (struct my_struct*)malloc(sizeof(struct my_struct));
-		s->word = token;
-		temp = (nodePtr)malloc(sizeof(node));
-		temp->fileName = fileName;
-		temp->frequency = 1;
-		temp->next = NULL;
-		s->list = temp;
-		HASH_ADD_KEYPTR(hh, words, token,strlen(token), s);	
-	}
-
 	/*HASH_FIND_STR(words, token, s);*/
 	/*printf("token before while is %s\n", token);*/
 	/*if(s) printf("token's frequency is%d\n", s->list->frequency);	*/
 	while(token != NULL)
 	{
 		/*printf("token is %s\n", token);	*/
-		token = TKGetNextToken(t);
 		HASH_FIND_STR(words, token, s);
 		printf("token is %s\n", token);
 		if(!s)
 		{
 			s = (struct my_struct*)malloc(sizeof(struct my_struct));
 			s->word = token;
-			temp = (nodePtr)malloc(sizeof(struct my_struct));
+			temp = (recordPtr)malloc(sizeof(struct record));
 			temp->fileName = fileName;
 			temp->frequency = 1;
 			temp->next = NULL;
+			temp->prev = NULL;
 			s->list = temp;
 			HASH_ADD_KEYPTR(hh, words, token, strlen(token), s);
 			continue;
 		}
 		if(s)
 		{
-			/*traverse list and change frequency*/
-			break;
+			s->list = updateRecordList(s->list, fileName);
 		}
+
+		token = TKGetNextToken(t);
 	}
 
 
@@ -387,4 +380,68 @@ int split(FILE* file, char* fileName) {
 	/*printf("outside of loop\n");*/
 
 	return 0;
+}
+
+recordPtr updateRecordList(recordPtr head, char* fname)
+{
+	recordPtr temp = head;
+	recordPtr curr = head;
+
+	while (curr){
+		if (strcmp(fname, curr->fileName) == 0){
+			curr->frequency++;
+			head = resortRecordList(head, curr);
+			return head;
+		}
+
+		temp = curr;
+		curr = curr->next;
+	}
+
+	recordPtr newRecord = (recordPtr)malloc(sizeof(struct record));
+	newRecord->frequency = 1;
+	newRecord->fileName = fname;
+	newRecord->prev = temp;
+	newRecord->next = NULL;
+	temp->next = newRecord;
+	return head;
+}
+
+
+recordPtr resortRecordList(recordPtr head, recordPtr curr)
+{
+	if (curr == head){
+		return head;
+	}
+
+	recordPtr temp = curr->prev;
+
+	while (temp->prev){
+		if (curr->frequency > temp->frequency){
+			temp->next = curr->next;
+			if(curr->next){
+				curr->next->prev = temp;
+			}
+			curr->next = temp;
+			curr->prev = temp->prev;
+			temp->prev->next = curr;
+			temp->prev = curr;
+			temp = curr->prev;
+		} else {
+			break;
+		}
+	}
+
+	if (temp == head && curr->frequency > temp->frequency){
+                temp->next = curr->next;
+		if (curr->next){
+			curr->next->prev = temp;
+		}
+                curr->next = temp;
+                temp->prev = curr;
+                curr->prev = NULL;
+                return curr;
+        }
+
+	return head;	
 }
