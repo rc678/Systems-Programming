df94c1fe63ffe7d6d824f7a0cf73e3fb8ccf9423
rosheen chaudhry <rc678@kill.cs.rutgers.edu>
2014-04-08 00:21:25 -0400

    type your commit message here

--- Multithreading/order.c
+++ Multithreading/order.c
@@ -3,3 +3,27 @@
 #include <pthread.h>
 #include <string.h>
 
+int main(int argc, char** argv)
+{
+
+	if(argc < 4)
+	{
+		printf("Too few arguements\n");
+		return 0;
+	}
+
+	if(argc > 4)
+	{
+		printf("Too many arguements\n");
+		return 0;
+	}
+
+	char* database = argv[1];
+	char* order = argv[2];
+	char* categories = argv[3];
+
+		
+
+	return 0;
+}
+
