8b5ff3c585f412f9cf7dcb4979f88d54eb23d63c
davidrubin9 <djr190@eden.rutgers.edu>
2014-04-11 17:37:54 -0400

    createQueues added
    doesn't compile though and im not sure what the problem is just yet

--- Multithreading/order.c
+++ Multithreading/order.c
@@ -1,3 +1,4 @@
+#include "uthash.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
@@ -13,32 +14,88 @@ typedef struct qNode{
 typedef struct Queue{
 	qNodePtr head;
 	qNodePtr tail;
-}Queue, QueuePtr;
+	int numOrders;
+}Queue, *QueuePtr;
 
-qNodePtr enqueue(Queue q, qNodePtr newNode)
+struct my_struct{
+        const char* category;
+        QueuePtr q;
+        UT_hash_handle hh; 
+};
+
+struct my_struct* categories;
+
+qNodePtr enqueue(QueuePtr q, qNodePtr newNode)
 {
 	if (q->head == NULL){
 		q->head = newNode;
 		q->tail = newNode;
+		q->numOrders++;
 		return q->head;
 	}
 	
 	q->tail->next = newNode;
 	q->tail = newNode;
+	q->numOrders++;
 	return q->head;
 }
 
-qNodePtr dequeue(Queue q)
+qNodePtr dequeue(QueuePtr q)
 {
+	if (q->head == NULL){
+		return q->head;
+	}
 	qNodePtr temp = q->head;
 	q->head = q->head->next;
+	q->numOrders--;
 	return temp;
+}
+
+/*reads categories.txt and adds a queue for each category to the hashtable*/
+void createQueues(char* categories)
+{
+	FILE* cg;
+	cg = fopen(categories, "r");
+	char* line;
+	char category[100];
+	int len;
+	struct my_struct *s;
+
+	if (cg == NULL){
+		printf("category file does not exist\n");
+		exit(-1);
+	}
+
+	if (fgetc(cg) == EOF){
+		printf("category file is empty\n");
+		exit(-1);
+	}
+	rewind(cg);
+
+	while(line = fgets(category, 100, cg))
+	{
+		if (line == NULL){
+			return;
+		}
+		
+		len = strlen(line);
+                line[len-1] = '\0';
+		char* c = malloc(sizeof(char));
+		strcpy(c, line);
+		s = (struct my_struct*)malloc(sizeof(struct my_struct));
+		s->category = c;
+		QueuePtr temp = (QueuePtr)malloc(sizeof(Queue));
+		s->q = temp;
+		HASH_ADD_KEYPTR(hh, categories, c, strlen(c), s);
+	}
+
+	fclose(cg);
 }
 
 /*makes a data structure based on the database.txt*/
 void createDatabase(char* database){
 	FILE* dbase;
-	
+
 	dbase = fopen(database, "r");
 	int size = 0;
 	char* input;
@@ -51,13 +108,13 @@ void createDatabase(char* database){
 	}
 
 	int c;
-	
+
 	if (fgetc(dbase) == EOF){
 		printf("database file is empty\n");
 		exit(-1);
 	}
 	rewind(dbase);
-	
+
 	/*counts the number of lines in the database.txt*/
 	do{
 		c = fgetc(dbase);
@@ -66,9 +123,9 @@ void createDatabase(char* database){
 			lineNum++;
 		}
 	}while(c != EOF);
-	
+
 	printf("%d\n", lineNum); 	
-	
+
 	qNode db[lineNum];
 
 	rewind(dbase);
@@ -77,15 +134,15 @@ void createDatabase(char* database){
 
 	while(in = fgets(line, 1000, dbase))
 	{
-	
-	if(in == NULL)
+
+		if(in == NULL)
 		{
 			break;
 		}
 
 		printf("%s\n", in);
 	}	
-		
+
 }
 
 int main(int argc, char** argv)
@@ -106,7 +163,7 @@ int main(int argc, char** argv)
 	char* database = argv[1];
 	char* order = argv[2];
 	char* categories = argv[3];
-	
+
 	if(database == NULL || order == NULL || categories == NULL){
 		printf("argument is null\n");
 		return 0;
