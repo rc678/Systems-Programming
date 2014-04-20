a2dbcd6cf04577ec78a92e2ef653a046c531931a
davidrubin9 <djr190@eden.rutgers.edu>
2014-04-20 16:03:31 -0400

    Added some comments

--- Multithreading/queue.h
+++ Multithreading/queue.h
@@ -1,9 +1,9 @@
 /*definition of a node for the queues*/
 typedef struct qNode{
 	double price;
-	double credRemain;
+	double credRemain; /*customers remaining credit after processed order*/
 	int customerID;
-	int noMoreOrders;
+	int noMoreOrders /*boolean to signify that all orders have been acounted for by producer*/
 	char* bookTitle;
 	const char* category;
 	struct qNode *next;
@@ -13,10 +13,10 @@ typedef struct qNode{
 typedef struct Queue{
 	qNodePtr head;
 	qNodePtr tail;
-	int numOrders;
+	int numOrders; /*current number of orders in queue*/
 	pthread_mutex_t mut;
-	pthread_cond_t nonEmpty;
-	pthread_cond_t nonFull;
+	pthread_cond_t nonEmpty; /*condition variable signifying there is data available to be processed in the queue*/
+	pthread_cond_t nonFull /*condition variable signifying there is space available to add data to the queue*/
 }Queue, *QueuePtr;
 
 /*adds item to the front of the queue and returns the queue*/
@@ -33,7 +33,7 @@ QueuePtr enqueue(QueuePtr q, qNodePtr newNode)
 	return q;
 }
 
-/*returns the item from the front of the queue*/
+/*removes item from the front of the queue and returns the item*/
 qNodePtr dequeue(QueuePtr q)
 {
 	if (q->head == NULL){
