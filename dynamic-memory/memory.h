2c7ba2022ace29f9e903a3a172539c2e7fab9aa5
davidrubin9 <djr190@eden.rutgers.edu>
2014-05-02 00:41:43 -0400

    edited comments

--- dynamic-memory/memory.h
+++ dynamic-memory/memory.h
@@ -1,8 +1,8 @@
 struct memEntry{
-	struct memEntry* prev;/*pointer to previous block*/
-	struct memEntry* next;/*pointer to next block*/
-	int free;/*is block free or not*/
-	int size;/*size of the memory block*/
+        struct memEntry* prev;/*pointer to previous memEntry in block*/
+        struct memEntry* next;/*pointer to next memEntry in block*/
+        int free;/*is block free or not--> 1 if yes and -1 if no*/
+        int size;/*size of the memory block*/
 };
 
 void* my_malloc(unsigned int size, char* file, int line);
